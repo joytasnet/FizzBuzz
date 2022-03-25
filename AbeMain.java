84d87ee32c0ccc33be87230e2d72cde0d5403539
ltbur <ltburtonst@gmail.com>
2022-03-25 14:40:19 +0900

    FizzBuzz追加

--- AbeMain.java
+++ AbeMain.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class AbeMain{
 	public static void main(String[] args){
-		for(int i=1;i<21;i++){
+		for(int i=1;i<=20;i++){
 			if(i % 3 == 0 && i % 5 == 0){
 				System.out.println("FizzBuzz");
 			}else if(i % 3 == 0){
@@ -9,7 +9,7 @@ public class AbeMain{
 			}else if(i % 5 == 0){
 				System.out.println("Buzz");
 			}else{
-			System.out.println(i);
+		System.out.println(i);
 			}
 		}
 	}
